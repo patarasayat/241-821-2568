569f3052e29881306b7fdc6df44604cc46351e6b
patarasayat <Patarasaya.t@ku.th>
2026-02-17 11:41:19 +0700

    add update

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,82 @@
-// ทำการ import http module เพื่อสร้าง server
-const http = require('http');
-const host = 'localhost';
-const port = 8000;
+const exprees = require('express');
+const bodyParser = require('body-parser');
+const app = exprees();
+const port = 8000
+let users = []
+let counter = 1;
+ 
+app.use(bodyParser.json());
+let use = [];
 
-//กำหนดค่าเริ่มต้นของ server เมื่อเปิดใช้งาน เว็บเบราว์เซอร์ ที่ localhost:8000
-
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    res.end('My Frist Server!');
-}
-//run server
-const sever = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log (`Server is running at htttp://${host}:${port}`);
+//path = GET /user
+app.get('/user', (req, res) => {
+    res.json(users);
+        
 });
+
+ //path = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+    users.push(user);
+    res.json({
+        message: 'User added successfully',
+        users: user });
+})
+
+// path = PUT /user/:id
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+// หา user จาก id  ที่ส่งมา
+    let selectUser = users.findIndex(user => user.id == id);
+
+    // อัพเดทข้อมูล user
+if(updatedUser.name){
+    users[selectIndex].name = updatedUser.name;
+
+
+}
+if(updatedUser.email){
+    users[selectIndex].email = updatedUser.email;
+}
+
+    //เอาข้อมูลที่ update ส่งกลับไป
+    res.json({
+        message: 'User updated successfully',
+        data: {
+            user: updatedUser,
+            indexUpdated: selectIndex
+        }
+    });
+})
+
+// path = DELETE /user/:id
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    // หา index ของ user ที่ต้องการลบจาก id ที่ส่งมา
+    let selectIndex = users.findIndex(user => user.id == id);
+   // ลบ user ออกจาก array โดยใช้ delete
+   users.splice(selectIndex, 1);
+
+    res.json({
+        message: 'User deleted successfully',
+            indexDeleted: selectIndex
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+    
+
+
+
+
+
+
+
+
+
+
